56fcee0cc81b89aa493d6c3ca0334b5732f1036c
Mauro Zegarra <mauro.zegarra@gmail.com>
2020-07-24 16:15:20 -0500

    05-add-DiffUtil

--- app/src/main/java/com/maurozegarra/example/sleeptracker/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/maurozegarra/example/sleeptracker/sleeptracker/SleepNightAdapter.kt
@@ -5,23 +5,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.maurozegarra.example.sleeptracker.R
 import com.maurozegarra.example.sleeptracker.convertDurationToFormatted
 import com.maurozegarra.example.sleeptracker.convertNumericQualityToString
 import com.maurozegarra.example.sleeptracker.database.SleepNight
 
-class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
-    var data = listOf<SleepNight>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
-
-    override fun getItemCount() = data.size
+//@formatter:off
+class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
+    //@formatter:on
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = data[position]
+        val item = getItem(position)
         holder.bind(item)
     }
 
@@ -60,5 +57,15 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
                 return ViewHolder(view)
             }
         }
+    }
+}
+
+class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
+    override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+        return oldItem.nightId == newItem.nightId
+    }
+
+    override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+        return oldItem == newItem
     }
 }
