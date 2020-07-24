89d49cc6c010427d0da532dc69ff4db600531c47
Mauro Zegarra <mauro.zegarra@gmail.com>
2020-07-24 10:07:28 -0500

    01-Add-Adapter

---
+++ app/src/main/java/com/maurozegarra/example/sleeptracker/sleeptracker/SleepNightAdapter.kt
@@ -0,0 +1,30 @@
+package com.maurozegarra.example.sleeptracker.sleeptracker
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.maurozegarra.example.sleeptracker.R
+import com.maurozegarra.example.sleeptracker.TextItemViewHolder
+import com.maurozegarra.example.sleeptracker.database.SleepNight
+
+class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
+    var data = listOf<SleepNight>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item = data[position]
+        holder.textView.text = item.sleepQuality.toString()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
+        return TextItemViewHolder(view)
+    }
+}
--- app/src/main/java/com/maurozegarra/example/sleeptracker/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/maurozegarra/example/sleeptracker/sleeptracker/SleepTrackerViewModel.kt
@@ -30,7 +30,7 @@ class SleepTrackerViewModel(val database: SleepDatabaseDao,
 
     private var tonight = MutableLiveData<SleepNight?>()
 
-    private val nights = database.getAllNights()
+    val nights = database.getAllNights()
 
     // Converted nights to Spanned for displaying.
     val nightsString: LiveData<Spanned> = Transformations.map(nights) { nights ->
